3a14ab919f55ab760795bef3bc46dfc08f24ccfd
sri nithya <nimishakavisrinithya60@gmail.com>
2025-01-28 20:44:19 +0530

    Time: 0 ms (100%), Space: 8.8 MB (45.93%) - LeetHub

---
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.c
@@ -0,0 +1,8 @@
+int numberOfEmployeesWhoMetTarget(int* hours, int hoursSize, int target) {
+    int c=0;
+    for(int i=0; i<hoursSize; i++)
+    {
+        if(hours[i]>=target) c++;
+    }
+    return c;
+}
