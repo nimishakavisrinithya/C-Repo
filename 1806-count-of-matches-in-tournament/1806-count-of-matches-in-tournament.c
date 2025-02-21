3a6a4d2bc2f13832bffe38d67618e30343b01329
sri nithya <nimishakavisrinithya60@gmail.com>
2025-02-21 15:33:38 +0530

    Time: 3 ms (46.07%), Space: 7.8 MB (68.27%) - LeetHub

---
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.c
@@ -0,0 +1,18 @@
+int match(int t, int m){
+    //if(m==0) return 0;
+    if(t%2==0){
+         m= t/2;
+         if(m==0) return 0;
+    return m+match(t/2, m);
+    }
+    else{
+        m = (t-1)/2;
+        if(m==0) return 0;
+        return m+match(((t-1)/2)+1, m);
+    }
+    
+}
+
+int numberOfMatches(int n){
+return match(n,n);
+}
