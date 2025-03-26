13ed8ae54cb23130801a5d9494ee5364c51ac130
sri nithya <nimishakavisrinithya60@gmail.com>
2025-03-26 21:13:31 +0530

    Time: 0 ms (100%), Space: 17.3 MB (14.02%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -4,13 +4,7 @@ public:
         int c, k=0;
         string s1;
         for(string x : details){
-            c=0;
-            for(char y : x){
-                c++;
-                if(c==12){
                     s1 +=x.substr(11, 2);
-                }
-            }
         if(stod(s1)>60) k++;
         s1.clear();
         }
