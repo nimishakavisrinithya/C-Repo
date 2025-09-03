8d487e7001759effb9369390b5d8befc7341d727
sri nithya <nimishakavisrinithya60@gmail.com>
2025-09-03 10:34:39 +0530

    Time: 130 ms (81.33%), Space: 145.6 MB (73.45%) - LeetHub

--- 2117-find-original-array-from-doubled-array/2117-find-original-array-from-doubled-array.cpp
+++ 2117-find-original-array-from-doubled-array/2117-find-original-array-from-doubled-array.cpp
@@ -4,7 +4,7 @@ public:
        vector<int>vec;
        int n = changed.size();
        if(n%2!=0) return vec;
-      map<int,int>mpp;
+      unordered_map<int,int>mpp;
        sort(changed.begin(), changed.end());
        for(int i=0; i<n; i++) mpp[changed[i]]++;
        for(int i=0; i<n; i++){
